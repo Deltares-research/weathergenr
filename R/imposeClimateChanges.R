02202ded5cf0ac18e822bc78c1da42ff8baed597
tanerumit <tanerumit@gmail.com>
2022-10-17 10:36:16 +0200

    bug fix

--- R/imposeClimateChanges.R
+++ R/imposeClimateChanges.R
@@ -65,40 +65,21 @@ imposeClimateChanges <- function(
       function(m) rep(change.factor.precip.variance[m], max(year_ind)))
   }
 
-  # Set number of cores for parallel computing
-  if(compute.parallel == TRUE) {
-
-    if(is.null(num.cores)) num.cores <- parallel::detectCores()-1
-    cl <- parallel::makeCluster(num.cores)
-    doParallel::registerDoParallel(cl)
-    `%d%` <- foreach::`%dopar%`
-
-  } else {
-
-    `%d%` <- foreach::`%do%`
-  }
-
-  ##############################################################################
   ##############################################################################
 
-  precip <- foreach::foreach(x=seq_len(ngrids)) %d% {
+    # Apply climate changes (per grid)
+  for (x in 1:ngrids) {
 
-      weathergenr::quantileMapping(
+    # Perturb daily precipitation using quantile mapping
+    climate.data[[x]]$precip <- quantileMapping(
             value = climate.data[[x]]$precip,
             mon.ts = month_ind,
             year.ts = year_ind,
             mean.change = precip_meanf,
-            var.change = precip_varf,
-            fit.method = fit.method)
-  }
-
-  if(compute.parallel == TRUE) parallel::stopCluster(cl)
-
-  for (x in 1:ngrids) {
+            var.change = precip_varf)
 
     # Perturb temp, temp_min, and temp_max by delta factors
-    climate.data[[x]]$precip <- precip[[x]]
-    climate.data[[x]]$temp   <- climate.data[[x]]$temp + tempf2
+    climate.data[[x]]$temp <- climate.data[[x]]$temp + tempf2
     climate.data[[x]]$temp_min <- climate.data[[x]]$temp_min + tempf2
     climate.data[[x]]$temp_max <- climate.data[[x]]$temp_max + tempf2
 
@@ -112,8 +93,58 @@ imposeClimateChanges <- function(
 
   # Replace possible infinite/NA values with zero
   climate.data <- lapply(1:length(climate.data), function(y)
-        do.call(tibble::tibble, lapply(climate.data[[y]],
-          function(x) replace(x, is.infinite(x) | is.na(x), 0))))
+    do.call(tibble, lapply(climate.data[[y]], function(x) replace(x, is.infinite(x), 0))))
+
+
+  # # Set number of cores for parallel computing
+  # if(compute.parallel == TRUE) {
+  #
+  #   if(is.null(num.cores)) num.cores <- parallel::detectCores()-1
+  #   cl <- parallel::makeCluster(num.cores)
+  #   doParallel::registerDoParallel(cl)
+  #   `%d%` <- foreach::`%dopar%`
+  #
+  # } else {
+  #
+  #   `%d%` <- foreach::`%do%`
+  # }
+
+  ##############################################################################
+  ##############################################################################
+
+  # precip <- foreach::foreach(x=seq_len(ngrids)) %d% {
+  #
+  #     weathergenr::quantileMapping(
+  #           value = climate.data[[x]]$precip,
+  #           mon.ts = month_ind,
+  #           year.ts = year_ind,
+  #           mean.change = precip_meanf,
+  #           var.change = precip_varf,
+  #           fit.method = fit.method)
+  # }
+  #
+  # if(compute.parallel == TRUE) parallel::stopCluster(cl)
+  #
+  # for (x in 1:ngrids) {
+  #
+  #   # Perturb temp, temp_min, and temp_max by delta factors
+  #   climate.data[[x]]$precip <- precip[[x]]
+  #   climate.data[[x]]$temp   <- climate.data[[x]]$temp + tempf2
+  #   climate.data[[x]]$temp_min <- climate.data[[x]]$temp_min + tempf2
+  #   climate.data[[x]]$temp_max <- climate.data[[x]]$temp_max + tempf2
+  #
+  #   if(isTRUE(calculate.pet)) {
+  #     climate.data[[x]]$pet <- with(climate.data[[x]], hargreavesPet(
+  #         months = month_ind, temp = temp, tdiff = temp_max - temp_min,
+  #         lat = climate.grid$y[x]))
+  #   }
+  #
+  # }
+  #
+  # # Replace possible infinite/NA values with zero
+  # climate.data <- lapply(1:length(climate.data), function(y)
+  #       do.call(tibble::tibble, lapply(climate.data[[y]],
+  #         function(x) replace(x, is.infinite(x) | is.na(x), 0))))
 
   return(climate.data)
 
