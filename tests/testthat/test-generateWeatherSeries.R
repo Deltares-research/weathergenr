06604c7d899ff7a850be038e724ec8c06687798e
tanerumit <tanerumit@gmail.com>
2023-10-13 19:02:13 +0200

    unit test for generateWeatherSeries

--- tests/testthat/test-generateWeatherSeries.R
+++ tests/testthat/test-generateWeatherSeries.R
@@ -2,17 +2,26 @@
 
 testthat::test_that("check if the output dimensions are correct", {
 
-  output <- generateWeatherSeries(
+ sim_year_start = 2020
+ sim_year_num = 20
+ month_start = 1
+ realization_num = 2
+
+ date_begin <- as.Date(paste(sim_year_start, month_start, "01", sep = "-"))
+ date_end <- as.Date(paste(sim_year_start+sim_year_num, month_start, "01", sep = "-"))-1
+ date_vector <- seq.Date(date_begin, date_end, by = "day")
+
+ output <- generateWeatherSeries(
     weather.data = ncdata$data,
     weather.grid = ncdata$grid,
     weather.date = ncdata$date,
     variable.names = c("precip", "temp", "temp_min", "temp_max"),
     variable.labels = c("precip", "temp", "temp_min", "temp_max"),
     variable.units = NULL,
-    sim.year.num = 20,
-    sim.year.start = 2020,
-    month.start = 1,
-    realization.num = 2,
+    sim.year.num = sim_year_num,
+    sim.year.start = sim_year_start,
+    month.start = month_start,
+    realization.num = realization_num,
     warm.variable = "precip",
     warm.signif.level = 0.90,
     warm.sample.num = 1000,
@@ -25,7 +34,10 @@ testthat::test_that("check if the output dimensions are correct", {
     output.path = tempdir(),
     seed = 123)
 
-  expect_equal(nrow(output[[1]]), 20 * 365)
-  expect_equal(length(output[[2]]), 20 * 365)
+  # Check if the resampled dates are from the historic days
+  expect_true(all(sapply(1:realization_num, function(x) output$resampled[[x]] %in% ncdata$date)))
+
+  # Check future date vector
+  expect_equal(output$dates, date_vect)
 
 })
