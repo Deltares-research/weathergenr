232ba9b65987c6119582ba5350290953ef76eeef
Umit Taner <umit.Taner@deltares.nl>
2024-10-18 17:34:09 +0200

    add climateSurface function

--- R/climateSurface.R
+++ R/climateSurface.R
@@ -41,7 +41,7 @@ climateSurface <- function(
     plot.title = "change in variable",
     variable.x.label = "change in temperature",
     variable.y.label = "change in precipitation",
-    failure.direction = "low",
+    failure.direction = 1,
     gcm.scenario.list = c("rcp26", "rcp45", "rcp60", "rcp85"),
     gcm.bivariate.dist = FALSE,
     gcm.transparency = 0.85,
@@ -63,7 +63,7 @@ climateSurface <- function(
       theme_light() %+replace%
       theme(
         plot.background = element_rect(fill = "white"),
-        plot.title = element_text(size = size+2, hjust = 0),
+        plot.title = element_text(size = size+2, hjust = 0, margin = margin(0,0,-10,0)),
         axis.ticks = element_line(colour = "gray60"),
         axis.title = element_text(size = size),
         axis.text = element_text(size = size-2),
@@ -91,12 +91,15 @@ climateSurface <- function(
     # Specify z range and breaks
     if(is.null(variable.z.min)) variable.z.min <- min(str.data[[variable.z]])
     if(is.null(variable.z.max)) variable.z.max <- max(str.data[[variable.z]])
-    z_breaks <- seq(variable.z.min, variable.z.max, length.out = variable.z.bin)
+    z_breaks <- seq(variable.z.min, variable.z.max, length.out = variable.z.bin+1)
 
     # Specify z legend
     if(is.null(variable.z.min.legend)) variable.z.min.legend  <- variable.z.min
     if(is.null(variable.z.max.legend)) variable.z.max.legend  <- variable.z.max
     if(is.null(variable.z.bin.legend)) variable.z.bin.legend  <- variable.z.bin
+
+    z_legend_breaks <- pretty(z_breaks, variable.z.bin.legend)
+    z_legend_limits <- range(z_legend_breaks)
 
     # Set threshold to mean if not specified
     if(is.null(threshold.z)) {
@@ -104,21 +107,23 @@ climateSurface <- function(
         pull(variable.z) %>% mean()
     }
 
-    bin_num <- length(z_breaks) - 1
-    mid_bin <- findInterval(threshold.z, z_breaks)
-    colpal <- vector("character", length(z_breaks)-1)
-    colpal[[mid_bin]] <- "white"
+
+    bin_num <- length(z_legend_breaks) - 1
+    mid_bin <- findInterval(threshold.z, z_legend_breaks)
+    colpal <- vector("character", length(z_legend_breaks)-1)
     bin_num_lw <- mid_bin-1
     bin_num_up <- length(colpal) - mid_bin
 
     # Set color palette based on direction of increasing performance
-    if (failure.direction == "low") {
-      colpal[1:bin_num_lw] <- colorRampPalette(c("#99000D", "#FEE5D9"))(bin_num_lw)
-      colpal[(mid_bin+1):length(colpal)]  <- colorRampPalette(c("#EFF3FF", "#004B88"))(bin_num_up)
+    if (failure.direction == 1) {
+      colpal[1:bin_num_lw] <- colorRampPalette(c("#FF3300", "#FEE5D9"))(bin_num_lw)
+      colpal[(mid_bin+1):length(colpal)]  <- colorRampPalette(c("#EFF3FF", "#0033FF"))(bin_num_up)
     } else {
-      colpal[1:bin_num_lw] <- colorRampPalette(c("#004B88", "#EFF3FF"))(bin_num_lw)
-      colpal[(mid_bin+1):length(colpal)]  <- colorRampPalette(c("#FEE5D9","#99000D"))(bin_num_up)
+      colpal[1:bin_num_lw] <- colorRampPalette(c("#0033FF", "#EFF3FF"))(bin_num_lw)
+      colpal[(mid_bin+1):length(colpal)]  <- colorRampPalette(c("#FEE5D9","#FF3300"))(bin_num_up)
     }
+
+    colpal[[mid_bin]] <- "white"
 
     # Core climate response surface
     p <- ggplot(str.data, aes(x = .data[[variable.x]], y = .data[[variable.y]])) +
@@ -126,22 +131,23 @@ climateSurface <- function(
       gg_theme_surface() +
       # Place z dimension
       geom_contour_filled(aes(z = .data[[variable.z]],
-            fill = after_stat(level_mid)), breaks = z_breaks) +
+            fill = after_stat(level_mid)), breaks = z_legend_breaks) +
       # Place threshold line
       geom_contour(aes(z = .data[[variable.z]]),
-                   breaks = threshold.z, color = "black", size = 1) +
+                   breaks = threshold.z, color = "black", linewidth = 1) +
       # Set x,y, and fill scales
       scale_x_continuous(expand = c(0, 0), breaks = variable.x.breaks, labels = ~ paste0(.x, "%")) +
       scale_y_continuous(expand = c(0, 0), breaks = variable.y.breaks, labels = ~ paste0(.x, "°C")) +
       scale_fill_gradientn(colors = colpal,
-                           breaks = pretty(z_breaks, variable.z.bin.legend),
-                           limits = range(pretty(z_breaks, variable.z.bin.legend)),
+                           breaks = z_legend_breaks,
+                           limits = z_legend_limits,
                            guide = guide_colorbar(barwidth=25, show.limits=TRUE, ticks.colour = "black",
                                               barheight = 1.30*text.scale, order = 1,
                                               draw.ulim = TRUE, draw.llim = TRUE)) +
       # Set labs
       labs(x = variable.x.label,y = variable.y.label,
         color = "Climate\nProjections", fill = "", title = plot.title)
+
 
       ######## GCM Dots ########################################################
 
@@ -156,7 +162,9 @@ climateSurface <- function(
             color = scenario, shape = horizon), data = gcm.data,
             stroke = 1.5, size = 2*text.scale, alpha = gcm.transparency) +
             scale_color_manual(values = gcm_scenario_color) +
-            scale_shape_manual(values = gcm_period_shape)
+            scale_shape_manual(values = gcm_period_shape) +
+            labs(shape = "Time\nHorizon")
+
 
         # Draw GCM legend
         if(isTRUE(gcm.legend)) {
@@ -193,27 +201,4 @@ climateSurface <- function(
     return(p)
 
   }
-
-
-# #Plot marginal distributions
-# if(gcm.marginal.dist == TRUE & !is.null(gcm.data)) {
-#
-#     p_top  <- ggplot(gcm.data, aes(x = .data[[variable.x]]))+
-#       scale_fill_manual(values = gcm_scenario_color) +
-#       scale_color_manual(values = gcm_scenario_color) +
-#       geom_density(aes(fill = scenario, color = scenario), alpha = 0.4, position="identity") +
-#       scale_x_continuous(expand = c(0, 0), limits = range(variable.x.breaks)) +
-#       gg_theme_blank + guides(fill="none", color = "none")
-#
-#     p_left  <- ggplot(gcm.data, aes(y = .data[[variable.y]]))+
-#       scale_fill_manual(values = gcm_scenario_color) +
-#       scale_color_manual(values = gcm_scenario_color) +
-#       geom_density(aes(fill = scenario, color = scenario), alpha = 0.4, position="identity") +
-#       scale_y_continuous(expand = c(0, 0), limits = range(variable.y.breaks)) +
-#       gg_theme_blank + guides(fill="none", color = "none") +
-#       scale_x_reverse()
-#
-#     p <- empty + p_top + p_left + p + plot_layout(ncol = 2, widths = c(1, 7), heights = c(1,7))
-#
-#   }
 
